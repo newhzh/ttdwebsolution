b7cf90e329ca63536404ea44ee28ae450926c585
newhzh <newhzh@163.com>
2014-03-10 16:50:33 +0800

    增加产品列表页面

--- TTDWeb/Controllers/HomeController.cs
+++ TTDWeb/Controllers/HomeController.cs
@@ -15,18 +15,28 @@ namespace TTDWeb.Controllers
         }
         #endregion
 
-        #region 产品列表  hahahhaha
-        public ActionResult ProductList()
+        #region 产品列表 
+        public ActionResult ProductList(string type,string money,string term)
         {
+            string sql = "";
+            
+            
             return View();
 
         }
         
         #endregion
 
+        #region 产品详情页
         public ActionResult Detail(string productid)
         {
             return View();
         }
+        #endregion
+
+        #region 车贷申请
+
+        #endregion
+
     }
 }
