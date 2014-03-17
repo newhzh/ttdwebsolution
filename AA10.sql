c7d2f7447f6a5ee40178e24f43257920c37d739a
clzttt <clzlie@gmail.com>
2014-03-17 17:17:25 +0800

    AA10.sql 编辑

--- AA10.sql
+++ AA10.sql
@@ -16,3 +16,61 @@ values ('sCarProperty', '1', '车贷-房产', '本人名下有本地商品房（有房产证）', nu
 
 insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
 values ('sCarProperty', '2', '车贷-房产', '父母、配偶名下有本地商品房（有房产证）', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sCarPurchasingPeriod', '0', '购车阶段', '未选好车型', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sCarPurchasingPeriod', '1', '购车阶段', '准备去4S店看车', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sCarPurchasingPeriod', '2', '购车阶段', '已经去4S店看过车', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sFirmProperty', '0', '是否有本地商品房', '没有本地商品房', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sFirmProperty', '1', '是否有本地商品房', '本人名下有本地商品房（有房产证）', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslSalaryType', '0', '工资发放形式', '公司发到银行工资卡', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslSalaryType', '1', '工资发放形式', '领取现金', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslCreditOwner', '0', '您是否有信用卡', '有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslCreditOwner', '1', '您是否有信用卡', '没有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslCardNo', '0', '是否有负债（信用卡）', '有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslCardNo', '1', '是否有负债（信用卡）', '没有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslLoan', '0', '您之前是否成功申请贷款', '有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslLoan', '1', '您之前是否成功申请贷款', '没有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslLoanDue', '0', '是否有负债（贷款）', '有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sPerslLoanDue', '1', '是否有负债（贷款）', '没有', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sHouseLocalorNot', '0', '户籍', '本地', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sHouseLocalorNot', '1', '户籍', '外地', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sHouseNew', '0', '是否二手房', '新房', null, null);
+
+insert into AA10 (AAA100, AAA102, AAA101, AAA103, AAA104, AAA105)
+values ('sHouseNew', '1', '是否二手房', '二手房', null, null);
+
