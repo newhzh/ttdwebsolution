badc58c567b779262f4366f7b16ab2dfffcdd4f1
newhzh <newhzh@163.com>
2014-04-12 13:30:19 +0800

    login completed by hzh

--- TTDWeb/Controllers/AccountController.cs
+++ TTDWeb/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using TTDWeb.Models;
 
 namespace TTDWeb.Controllers
 {
--- TTDWeb/Models/LoginModel.cs
+++ TTDWeb/Models/LoginModel.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
-namespace TTDWeb.Controllers
+namespace TTDWeb.Models
 {
     public class LoginModel
     {
+        [Required]
+        [Display(Name = "用户名")]
         public string UserName { get; set; }
-        public bool RememberMe { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
         public string Password { get; set; }
+
+        [Display(Name = "记住登录账户?")]
+        public bool RememberMe { get; set; }
     }
 }
