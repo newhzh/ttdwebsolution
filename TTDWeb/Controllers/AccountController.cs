3f6f66191d08463377cecbbb8b24ff141cc219af
newhzh <newhzh@163.com>
2014-04-14 09:27:22 +0800

    注册功能加入地区选择

--- TTDWeb/Controllers/AccountController.cs
+++ TTDWeb/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using SqlServerDAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using TTDWeb.Common;
 using TTDWeb.Models;
 
 namespace TTDWeb.Controllers
@@ -21,10 +24,212 @@ namespace TTDWeb.Controllers
 
         #region 信贷经理·注册
 
+        [AllowAnonymous]
         public ActionResult Reg()
         {
+            //读取省级地区列表
+            List<SelectListItem> items = GetRegionList("0", "");
+            SelectList list = new SelectList(items,"Value","Text","--请选择--");
+            ViewBag.Province = list;
+            ViewBag.City = new SelectList(GetRegionList("0", "-99"), "Value", "Text", "--请选择--");//传空值
+            ViewBag.County = new SelectList(GetRegionList("0", "-99"), "Value", "Text", "--请选择--");//传空值
+            ViewBag.Organ = new SelectList(GetOrganList("-1"), "Value", "Text", "--请选择--");//传空值
+
             return View();
         }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                DA_Custom daCustom = new DA_Custom();
+                string err = "";
+                Message.CustomInfo info = new Message.CustomInfo();
+
+                #region 信息类Info赋值
+                info.NickName = model.NickName;
+                info.CellPhone = model.CellPhone;
+                info.Pwd = model.Password;
+                info.CustomName = model.CustomName;
+                info.Email = model.Email;
+
+                info.Address = "";
+                info.Balance = 0;
+                info.Birthday = Convert.ToDateTime("1900-01-01");
+
+                info.CertApproveDate = Convert.ToDateTime("1900-01-01");
+                info.CertApproveManName = "";
+                info.CertGrade = "";
+                info.CertNum = "";
+                info.CertState = "0";
+                info.CertType = "";
+                info.City = "";
+                info.CreditGradeBorrow = "01";//默认初级
+                info.CreditGradeInvest = "01";//默认初级
+                info.CreditScoreBorrow = 0;
+                info.CreditScoreInvest = 0;
+                info.CustomCode = "";
+                info.CustomID = "";
+
+                info.CustomState = "1";
+                info.CustomType = "0";
+                info.Fax = "";
+                info.FileNameCopy1 = "";
+                info.FileNameCopy2 = "";
+                info.FileNameCopy3 = "";
+                info.FileNameCopy4 = "";
+                info.LastLoginDate = DateTime.Today;    //后台不会用到此值
+
+                info.LinkManName = "";
+                info.LinkManPhone = "";
+                info.Memo = "";
+
+                info.PC = "";
+                info.PhotoFileName = "";
+                info.Points = 0;
+                info.Province = "";
+
+                info.PYM = "";
+                info.RegDate = DateTime.Today;
+                info.RegWay = "0";  //0表示网站注册
+                info.ServerStaffID = "";
+                info.Sex = "";
+                info.Tel = "";
+                info.OutletDptID = "";
+                #endregion
+
+                if (daCustom.Insert(ref info, ref err, null))
+                {
+                    //注册成功，转入提示页面
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("", err);
+                }
+            }
+
+            // 如果我们进行到这一步时某个地方出错，则重新显示表单
+            return View(model);
+        }
+
+        #region 验证手机号码是否可用
+        public void CheckCellPhone(string CellPhone)
+        {
+            DA_Common da = new DA_Common();
+            string sql = "select sCustomID from t_custom where sCellPhone='" + CellPhone + "'";
+            DataSet ds = da.CommonQuery(sql);
+            if (BizCommon.IsNullDataSet(ds))
+            {
+                Response.ContentType = "text/html";
+                Response.Write("恭喜，此手机可以注册!");
+            }
+            else
+            {
+                Response.ContentType = "text/html";
+                Response.Write("此手机已经存在，请换一个!");
+            }
+        }
+        #endregion
+
+        #region 验证邮箱是否可用
+        public void CheckEmail(string Email)
+        {
+            DA_Common da = new DA_Common();
+            string sql = "select sCustomID from t_custom where sEmail='" + Email + "'";
+            DataSet ds = da.CommonQuery(sql);
+            if (BizCommon.IsNullDataSet(ds))
+            {
+                Response.ContentType = "text/html";
+                Response.Write("恭喜，此邮箱可以注册!");
+            }
+            else
+            {
+                Response.ContentType = "text/html";
+                Response.Write("此邮箱已经存在，请换一个!");
+            }
+        }
+        #endregion
+
+        #region 验证昵称是否可用
+        public void CheckNickName(string NickName)
+        {
+            DA_Common da = new DA_Common();
+            string sql = "select sCustomID from t_custom where sNickName='" + NickName + "'";
+            DataSet ds = da.CommonQuery(sql);
+            if (BizCommon.IsNullDataSet(ds))
+            {
+                Response.ContentType = "text/html";
+                Response.Write("恭喜，此昵称可以注册!");
+            }
+            else
+            {
+                Response.ContentType = "text/html";
+                Response.Write("此昵称已经存在，请换一个!");
+            }
+        }
+        #endregion
+
+        #region 读取地区数据
+
+        List<SelectListItem> GetRegionList(string level, string parentid)
+        {
+            string sql = "select sRegionID,sRegionName,sParentID from t_region where nLevel=" + level;
+            if (parentid != "")
+                sql += " and sParentID='" + parentid + "'";
+
+            if (Convert.ToInt32(level) > 0 && parentid == "")
+                sql += " and 1=2";
+
+            DA_Common da = new DA_Common();
+            DataSet ds = da.CommonQuery(sql);
+
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "--请选择--", Value = "" });
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                items.Add(new SelectListItem { Text = dr["sRegionName"].ToString(), Value = dr["sRegionID"].ToString() });
+            }
+            return items;
+        }
+
+        public JsonResult GetRegionDataJson(string level,string parentid)
+        {
+            List<SelectListItem> items = GetRegionList(level, parentid);
+            return Json(items, JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion
+
+        #region 读取机构数据
+        List<SelectListItem> GetOrganList(string regionid)
+        {
+            string sql = "select sOrganID,sOrganName from t_foreignorgan where sregionid='" + regionid + "'";
+            if (regionid == "")
+                sql += " and 1=2";
+
+            DA_Common da = new DA_Common();
+            DataSet ds = da.CommonQuery(sql);
+
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "--请选择--", Value = "" });
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                items.Add(new SelectListItem { Text = dr["sOrganName"].ToString(), Value = dr["sOrganID"].ToString() });
+            }
+            return items;
+        }
+
+        public JsonResult GetOrganDataJson(string regionid)
+        {
+            List<SelectListItem> items = GetOrganList(regionid);
+            return Json(items, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
 
         #endregion
 
---
+++ TTDWeb/Models/RegisterModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TTDWeb.Models
+{
+    #region 注册模型
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "手机")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$)", ErrorMessage = "手机号码格式非法!")]
+        public string CellPhone { get; set; }
+        
+        [Required]
+        [Display(Name = "姓名")]
+        public string CustomName { get; set; }
+
+        [Required]
+        [Display(Name = "省份")]
+        public string Province { get; set; }
+
+        [Required]
+        [Display(Name = "城市")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "区县")]
+        public string County { get; set; }
+
+        [Required]
+        [Display(Name = "机构")]
+        public string OrganID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "确认密码")]
+        [Compare("Password", ErrorMessage = "密码和确认密码不匹配。")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "电子邮箱")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("\\w+(\\.\\w+)*@\\w+(\\.\\w+)+", ErrorMessage = "电子邮箱格式非法!")]
+        public string Email { get; set; }
+
+        [Display(Name = "昵称")]
+        public string NickName { get; set; }
+
+        public string yzm { get; set; }
+    }
+    #endregion
+}
